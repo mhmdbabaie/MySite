5ccd11e4f657b5e1153d36844cb5748599c75575
MohammadHosein BabaAli <110888361+mhmdbabaie@users.noreply.github.com>
2022-09-07 16:22:43 +0430

    DataBase
    the admin panel created and the model completed

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from blog.models import Post
 
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = 'empty'
+    list_display = ('title','counted_views','status','published_date')
+    ordering = ('created_date',)
+admin.site.register(Post,PostAdmin)
--- website/models.py
+++ website/models.py
@@ -1,3 +1,13 @@
+import email
 from django.db import models
 
 # Create your models here.
+
+
+class contact(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    subject = models.CharField(max_length=100)
+    message = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
