eca3e1bfd2ce070f65fac231536cbfaf5de8b9ce
mhmdbabaie <110888361+mhmdbabaie@users.noreply.github.com>
2022-09-08 20:06:37 +0430

    contactAdmin added
    msg

--- blog/admin.py
+++ blog/admin.py
@@ -7,5 +7,6 @@ class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
     empty_value_display = 'empty'
     list_display = ('title','counted_views','status','published_date')
-    ordering = ('created_date',)
+    list_filter = ['status']
+    search_fields = ('title','content')
 admin.site.register(Post,PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,8 @@ class Post(models.Model):
     published_date = models.DateTimeField(null=True)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ['-created_date']
 
 
     def __str__(self):
