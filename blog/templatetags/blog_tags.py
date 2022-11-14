92d4cef543595e7e56a78366e56ebda7f3ea5e8e
MohammadHosein BabaAli <110888361+mhmdbabaie@users.noreply.github.com>
2022-11-14 17:20:48 +0330

    simple tag
    total posts added to test page

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,64 @@
+from django import template
+from blog.models import Post 
+
+
+register = template.Library()
+
+
+@register.simple_tag(name='totalposts')
+def function():
+    posts = Post.objects.filter(status=1).count()
+    return posts
+
+
+
+@register.simple_tag(name='posts')
+def function():
+    posts = Post.objects.filter(status=1)
+    return posts
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# اینجا واسه قسمت سیمپل تگ میباشد که از طرق پیج تست کوِری میزنیم
