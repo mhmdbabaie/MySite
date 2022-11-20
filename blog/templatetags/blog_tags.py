fdac1b3494657507b46e51f2c4883fb6bacd84aa
MohammadHosein BabaAli <110888361+mhmdbabaie@users.noreply.github.com>
2022-11-20 17:14:13 +0330

    CATEGORY INCLUSION ADDED

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,14 +1,13 @@
 from django import template
 from blog.models import Post 
-register = template.Library()
+from blog.models import Category
 
+register = template.Library()
 
 @register.simple_tag(name='totalposts')
 def function():
     posts = Post.objects.filter(status=1).count()
     return posts
-
-
 
 @register.simple_tag(name='posts')
 def function():
@@ -19,10 +18,19 @@ def function():
 def snippet(value,args=100):
     return value[:args] + "..."
 
-@register.inclusion_tag('popularposts.html')
-def popularposts():
+@register.inclusion_tag('blog/blog-popular-posts.html') 
+def latestposts():
     posts = Post.objects.filter(status=1).order_by("published_date")
-    return {'posts': posts}
+    return {'posts': posts}  
+
+@register.inclusion_tag('blog/blog-post-categories.html')
+def postcategories():
+    posts = Post.objects.filter(status=1)
+    categories = Category.objects.all()
+    cat_dict ={}
+    for name in categories:
+        cat_dict[name] =posts.filter(Category=name).count()
+    return {'categories': cat_dict}
 
 
 
@@ -36,35 +44,5 @@ def popularposts():
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
 # اینجا واسه قسمت سیمپل تگ میباشد که از طرق پیج تست کوِری میزنیم
